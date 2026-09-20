b1c2a9ac656fe803fd5c96d806375944d52eccbd
Aleksander-Wi <aleksander.wilczak1@gmail.com>
2026-09-20 20:32:31 +0200

    Update HAY.py

--- modules/HAY.py
+++ modules/HAY.py
@@ -2,7 +2,6 @@ def HAY(wheat_loops):
 	for i in range(get_world_size() * wheat_loops):
 			if get_ground_type() == Grounds.Soil:
 				till()
-			use_item(Items.Fertilizer)
 			harvest()
 			if get_pos_y() == get_world_size() - 1:
 				move(East)
